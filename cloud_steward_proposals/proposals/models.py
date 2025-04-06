a6528e5013cdf71cf4fd7bf266baddd7cec57e7b
Benjamin Rogol <ben@thecloudsteward.com>
2025-04-06 19:21:51 -0400

    Improve admin 1

--- cloud_steward_proposals/proposals/models.py
+++ cloud_steward_proposals/proposals/models.py
@@ -1,4 +1,5 @@
 import uuid
+from decimal import Decimal
 from django.db import models
 from django.utils import timezone
 
@@ -7,17 +8,27 @@ def thirty_days_from_now():
 
 def generate_slug():
     # Generate an 8-character hexadecimal string.
-    # You could also use the full uuid if you prefer.
     return uuid.uuid4().hex[:8]
 
 class ClientPage(models.Model):
-    # The slug field will use generate_slug() to set a default value.
     slug = models.SlugField(unique=True, default=generate_slug)
-    client_name = models.CharField(max_length=200, blank=True)
     company_name = models.CharField(max_length=200, blank=True)
+    client_name = models.CharField(max_length=200, blank=True)
     project_name = models.CharField(max_length=200, blank=True)
-    project_details = models.TextField(blank=True)
-    project_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    project_notes = models.TextField(blank=True)
+    project_summary = models.TextField(blank=True)
+    project_objectives = models.TextField(blank=True)
+    project_only_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    project_with_subscription_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    tier_1_subscription_price = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True, default=Decimal('249.00')
+    )
+    tier_2_subscription_price = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True, default=Decimal('649.00')
+    )
+    tier_3_subscription_price = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True, default=Decimal('1475.00')
+    )
     is_consultant = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     expires_at = models.DateTimeField(default=thirty_days_from_now)

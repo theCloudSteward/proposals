372df15d1b52088e0780f561539cbdb7fa8590ef
Benjamin Rogol <ben@thecloudsteward.com>
2025-04-06 19:51:14 -0400

    Update ProjectDetails for new data 1

--- frontend/src/components/ProjectDetails.jsx
+++ frontend/src/components/ProjectDetails.jsx
@@ -6,19 +6,23 @@ function ProjectDetails({data}) {
           <h2 className="text-2xl font-bold mb-4 text-gray-800">
             {data.project_name}
           </h2>
-          <h3 className="text-gray-700 mb-2">
-            {data.company_name}
+          <h3 className="mb-2 font-bold">
+            <strong>Prepared for:</strong> {data.company_name}
           </h3>
-          <p className="text-gray-700 mb-2">
+          <p className="mb-2 font-bold">
             <strong>Contact:</strong> {data.client_name}
           </p>
           <div
             className="prose mb-6 text-gray-800"
             dangerouslySetInnerHTML={{ __html: data.project_summary }}
           />
-          <p className="text-gray-700 mb-2">
-            <strong>Objectives</strong> {data.project_objectives}
-          </p>
+          <div>
+            <h3 className="mb-2 font-bold">Objectives</h3>
+            <div
+              className="prose mb-6 text-gray-800"
+              dangerouslySetInnerHTML={{ __html: data.project_summary }}
+            />
+          </div>
       </div>
   );
 }

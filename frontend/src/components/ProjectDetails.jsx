5d70b7961e3c322cfb25bfb022af176e504bca8e
Benjamin Rogol <ben@thecloudsteward.com>
2025-04-06 19:46:10 -0400

    Update ProjectDetails for new data

--- frontend/src/components/ProjectDetails.jsx
+++ frontend/src/components/ProjectDetails.jsx
@@ -1,33 +1,25 @@
 import React from 'react';
 
-function ProjectDetails({
-  data,
-  originalProjectPrice,
-  isProjectOnly,
-  setSelectedOption,
-  selectedOption
-}) {
+function ProjectDetails({data}) {
   return (
       <div className="max-w-5xl mx-auto px-4 py-8">
           <h2 className="text-2xl font-bold mb-4 text-gray-800">
             {data.project_name}
           </h2>
+          <h3 className="text-gray-700 mb-2">
+            {data.company_name}
+          </h3>
           <p className="text-gray-700 mb-2">
-            <strong>Client:</strong> {data.client_name}
+            <strong>Contact:</strong> {data.client_name}
           </p>
-          <p className="text-gray-700 mb-2">
-            <strong>Company:</strong> {data.company_name}
-          </p>
-          <p className="text-gray-700 mb-2">
-            <strong>Regular Project Price:</strong> $
-            {originalProjectPrice.toFixed(2)}
-          </p>
-
           <div
             className="prose mb-6 text-gray-800"
-            dangerouslySetInnerHTML={{ __html: data.project_details }}
+            dangerouslySetInnerHTML={{ __html: data.project_summary }}
           />
-        </div>
+          <p className="text-gray-700 mb-2">
+            <strong>Objectives</strong> {data.project_objectives}
+          </p>
+      </div>
   );
 }
 

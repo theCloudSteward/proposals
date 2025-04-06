27b0cb3057e68884a31cef06950bd16e775a5096
Benjamin Rogol <ben@thecloudsteward.com>
2025-04-06 19:29:53 -0400

    Improve admin 2

--- cloud_steward_proposals/proposals/admin.py
+++ cloud_steward_proposals/proposals/admin.py
@@ -3,4 +3,20 @@ from .models import ClientPage
 
 @admin.register(ClientPage)
 class ClientPageAdmin(admin.ModelAdmin):
-    list_display = ('slug', 'client_name','company_name', 'project_name','project_details', 'project_price', 'is_consultant', 'created_at', 'expires_at')
+    list_display = (
+        'slug',
+        'company_name',
+        'client_name',
+        'project_name',
+        'project_notes',
+        'project_summary',
+        'project_objectives',
+        'project_only_price',
+        'project_with_subscription_price',
+        'tier_1_subscription_price',
+        'tier_2_subscription_price',
+        'tier_3_subscription_price',
+        'is_consultant',
+        'created_at',
+        'expires_at'
+    )

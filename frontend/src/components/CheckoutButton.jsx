6035f8ac2f00c1d1547a2a858f180026bab9d76f
Benjamin Rogol <ben@thecloudsteward.com>
2025-04-08 19:03:56 -0400

    Switch to Stripe buttons 8

--- frontend/src/components/CheckoutButton.jsx
+++ frontend/src/components/CheckoutButton.jsx
@@ -24,11 +24,11 @@ function CheckoutButton({ slug, option, title }) {
     <div className="flex flex-col items-center">
       <button
         onClick={handleClick}
-        className="px-4 py-2 mt-12 bg-opacity-80 bg-blue-500 text-white shadow-md rounded hover:bg-blue-700"
+        className="px-4 py-2 mt-12 bg-opacity-90 bg-blue-500 text-white shadow-md rounded hover:bg-blue-800"
       >
         {title}
       </button>
-      <p className="italic text-xs mt-2 text-gray-400">Secure Checkout with Stripe</p>
+      <p className="italic text-xs mt-6 text-gray-400">Secure Checkout with Stripe</p>
     </div>
   );
 }

1aa7e030eb4228fe13fa782d1dae826d2ceb748f
Benjamin Rogol <ben@thecloudsteward.com>
2025-04-09 16:48:27 -0400

    Improve metadata / favicons 9

--- cloud_steward_proposals/cloud_steward_proposals/urls.py
+++ cloud_steward_proposals/cloud_steward_proposals/urls.py
@@ -1,41 +1,23 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
-from django.views.generic import View
-from django.http import HttpResponse, Http404
-from django.views.static import serve as static_serve
-from django.conf import settings
-import os
+from django.views.generic import TemplateView
+from django.views.static import serve
 from proposals.views import create_checkout_session, get_checkout_session_details
+import os
+from django.conf import settings
 
 # Path to your React build directory
 REACT_BUILD_DIR = os.path.join(settings.BASE_DIR, 'frontend', 'build')
-
-class ReactAppView(View):
-    def get(self, request, path='', *args, **kwargs):
-        file_path = os.path.join(REACT_BUILD_DIR, path)
-
-        if os.path.exists(file_path) and os.path.isfile(file_path):
-            return static_serve(request, path, document_root=REACT_BUILD_DIR)
-
-        index_path = os.path.join(REACT_BUILD_DIR, 'index.html')
-        if os.path.exists(index_path):
-            with open(index_path, encoding='utf-8') as f:
-                return HttpResponse(f.read(), content_type='text/html')
-
-        raise Http404("index.html not found")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/create-checkout-session/', create_checkout_session, name='create-checkout-session'),
     path('api/order/success/', get_checkout_session_details, name='order-success'),
     path('api/', include('proposals.urls')),
+    
+    # Explicitly serve favicon.ico from your React build folder.
+    path('favicon.ico', serve, {'document_root': REACT_BUILD_DIR, 'path': 'favicon.ico'}),
 
-    # Serve favicon.ico explicitly to avoid 404s or 500s
-    path('favicon.ico', static_serve, {
-        'document_root': REACT_BUILD_DIR,
-        'path': 'favicon.ico'
-    }),
-
-    # Catch-all to serve the React app
-    re_path(r'^(?P<path>.*)$', ReactAppView.as_view()),
+    # Fallback: serve index.html for all other non-API routes.
+    re_path(r'^(?!api/|c/success).*$', TemplateView.as_view(template_name='index.html')),
 ]

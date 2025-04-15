c100aefe4352e030ce6cab560e93c566e6a6639c
Benjamin Rogol <ben@thecloudsteward.com>
2025-04-14 20:30:59 -0400

    Debug success page error 6

--- cloud_steward_proposals/cloud_steward_proposals/urls.py
+++ cloud_steward_proposals/cloud_steward_proposals/urls.py
@@ -6,7 +6,7 @@ from proposals.views import create_checkout_session, get_checkout_session_detail
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/create-checkout-session/', create_checkout_session, name='create-checkout-session'),
-    path('api/order/success/', get_checkout_session_details, name='order-success'),
+    path('order/success/', get_checkout_session_details, name='order-success'),
     path('api/', include('proposals.urls')),
     
     # Catch-all for frontend routes (only after other routes are tried)

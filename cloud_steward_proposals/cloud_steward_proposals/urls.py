b2fe120ed8d997c385ab4828af570a1dec3db667
Benjamin Rogol <ben@thecloudsteward.com>
2025-04-14 20:32:06 -0400

    Debug success page error 8

--- cloud_steward_proposals/cloud_steward_proposals/urls.py
+++ cloud_steward_proposals/cloud_steward_proposals/urls.py
@@ -6,9 +6,10 @@ from proposals.views import create_checkout_session, get_checkout_session_detail
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/create-checkout-session/', create_checkout_session, name='create-checkout-session'),
-    path('order/success/', get_checkout_session_details, name='order-success'),
+    path('api/order/success/', get_checkout_session_details, name='order-success'),
     path('api/', include('proposals.urls')),
     
     # Catch-all for frontend routes (only after other routes are tried)
-    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
+    # re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?!api/).*$', TemplateView.as_view(template_name='index.html')),
 ]

b8473cdde2168c1d346c5c2e91d938a35d2950aa
Benjamin Rogol <ben@thecloudsteward.com>
2025-04-16 16:52:40 -0400

    Install Stripe email webhook upon checkout completion 13

--- cloud_steward_proposals/cloud_steward_proposals/settings.py
+++ cloud_steward_proposals/cloud_steward_proposals/settings.py
@@ -17,6 +17,7 @@ load_dotenv("/root/proposal.thecloudsteward/.env")
 # Fetch Stripe keys safely
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
+STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET", "")
 
 if not STRIPE_SECRET_KEY:
     raise ValueError("STRIPE_SECRET_KEY is missing! Check your .env file.")
